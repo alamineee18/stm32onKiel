24ee0efa44221a7152acfea685d3d0db24aec922
Md. Al-Amin Kabir <alaminkabir789@gmail.com>
2024-10-17 19:02:42 +0600

    update

--- GPIO_input/main.c
+++ GPIO_input/main.c
@@ -1,34 +1,35 @@
 #include "stm32f10x.h"                  // Device header
-
-void Delay(int time);
+#include "gp_drive.h"
+void Delays(int time);
 
 int main(void)
 {
-	RCC->APB2ENR |= 4;			//Enabling port A
-	RCC->APB2ENR |= 0x10;		//Enable Port C
-	
-	GPIOA->CRL &= 0xFFFFFFF0;		// Reset the port A pin 0
-	GPIOA->CRL |= 0x8;					// Set port A pin 0 as input push pull
-	
-	GPIOC->CRH &= 0xFF0FFFFF;
-	GPIOC->CRH |= 0x00300000;
+	init_GP(PA,0,IN,I_PP);
+	init_GP(PC,13,OUT50, O_GP_PP);
 	
 	while(1)
 	{
-		if(GPIOA->IDR & 0x00000001)
+		if(R_GP(PA,0)==1) /// Checking status of PIN ! portA 
 		{
-			Delay(10);
-			GPIOC->ODR ^=0x2000;
-			Delay(10);
+			Delays(10);
+			toggle_GP(PC,13); /// Toggle the PIN state
+			Delays(10);
+		}
+		else
+		{
+			W_GP(PC,13,1); /// Set the PIN 13 port C high
 		}
 	}
+	
 }
 
-void Delay (int time){
+
+void Delays(int time) /// Random delay function
+{
 	int t;
-	for(; time>0; time--)
+	for(;time>0;time--)
 	{
-		for(t = 0; t<100000; t++)
+	 for(t=0;t<100000;t++)
 		{}
 	}
 }
